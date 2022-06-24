7f3a10049683249dd6e2069b67b27501f93fb4ef
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2022-06-24 15:44:22 +0700

    Update api endpoint items
    Signed-off-by: Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>

--- api/items.php
+++ api/items.php
@@ -15,17 +15,56 @@
 
     $item = new Items();
     $arr = array();
-     try {
 
-        $items = $item->get_all_items();
-        $arr['error'] = false;
-        $arr['data'] = $items;
-        
-     } catch (Exception $e) {
-        $arr['error'] = true;
-        $arr['msg'] = "Can't get items";
-        $arr['data'] = null;
-     }
+    switch($_SERVER['REQUEST_METHOD']) {
+        case 'GET':
+            try {
 
+                if(isset($_GET['id'])) {
+                    $uid = $_GET['id'];
+                    $user_items = $item->get_items_user($uid);
+                    $not_taken_items = $item->get_not_taken_items($uid);
+                    $arr['error'] = false;
 
-     print_r(json_encode($arr));
+                    if(!empty($user_items)) {
+                        $arr['data']['items_user'] = $user_items;
+                    } else {
+                        $arr['data']['items_user'] = null;
+                    }
+
+                    if(!empty($not_taken_items)) {
+                        $arr['data']['not_taken'] = $not_taken_items;   
+                    } else {
+                        $arr['data']['not_taken'] = null;
+                    }
+
+                    print_r(json_encode($arr));
+                    break;
+                }
+
+                $items = $item->get_all_items();
+                $arr['error'] = false;
+                $arr['data'] = $items;
+                
+            } catch (Exception $e) {
+                $arr['error'] = true;
+                $arr['msg'] = "Can't get items";
+                $arr['data'] = null;
+            }
+            
+            print_r(json_encode($arr));
+
+            break;
+
+        default:
+            http_response_code(405);
+            print_r(json_encode(array(
+                "error" => true,
+                "msg" => "Method not allowed!"
+            )));
+            exit();
+    }
+
+ 
+     
+
